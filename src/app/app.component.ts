3d5d720a4e0977ec01ac2add3e5b7e006286c989
rahmam <mohammed.rahman@cdk.com>
2017-05-11 16:08:19 +0530

    App Compnent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app',
+    template: '<h1>Hello World</h1>'
 })
-export class AppComponent {
-  title = 'app works!';
-}
+
+export class AppComponent { }
